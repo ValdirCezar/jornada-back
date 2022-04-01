9253e4140ee008bc48622aa1423418356f8ac607
valdircezar <=>
2022-03-31 23:44:42 -0300

    feat: add user entity

---
+++ src/main/java/com/valdir/userservice/entities/User.java
@@ -0,0 +1,44 @@
+package com.valdir.userservice.entities;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "TB_USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+    private String password;
+    private String description;
+    private Integer age;
+    private Float score;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User person = (User) o;
+        return id != null && Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
